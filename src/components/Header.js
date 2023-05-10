46e8f64ee6d9b1fdd685287e7e80b3b04be835a5
Max Tuple <129517491+themaxtuple@users.noreply.github.com>
2023-05-10 16:18:33 -0400

    changed letter

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react"
 const Header = () => {
     return (
         <header>
-            <h1><a href="https://github.com/themaxtuple" target="_blank">Max Tuple</a>'s Portfolio</h1>
+            <h1><a href="https://github.com/themaxtuple" target="_blank">Max Tuple</a>'s portfolio</h1>
         </header>
     )
 }
